f792b732f31eaa03b4e721dda5d0ebb28f7077c4
Ashish Mishra <ashish.mishra.sse@gmail.com>
2024-08-25 18:54:55 +0530

    DataTypes-memoryconsumption,Range, mathemeaticalOperations

---
+++ src/variables/VariableConcepts.java
@@ -0,0 +1,38 @@
+package variables;
+
+public class VariableConcepts {
+    //Variables: Is like a jar/box/container which is
+    //           capable of holding/storing something
+
+    // WAP to perform addition of two integer numbers{10,12}
+
+    //Create variables
+    // Two ways of creating variables
+    //1. dataType variableName = value;
+
+    //byte, short, int, long, float, double, char, boolean
+
+    byte num1 = 127; // -128 to 127
+    short num2 = 32767; // -32768 to 32767
+    int num3 = 214748364; //-2147483648 to 2147483647
+
+    long num4 = 214748365;
+
+    int num5 = 4356789;
+
+    double f1 = 12.3333333333;
+
+
+    public static void main(String[] args) {
+
+        byte x =10;
+        int y =23;
+        int sum =x+y;
+        //x= x*5; cannot perform mathematical operations on byte and short data types
+
+
+    }
+
+
+}
+
